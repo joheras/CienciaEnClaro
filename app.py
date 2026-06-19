7d2eec29a3bcfa6d520f19d74268aa3fc159e36e
Jonathan <joheras@gmail.com>
2026-06-19 16:50:02 +0200

    app.py

--- app.py
+++ app.py
@@ -347,4 +347,4 @@ async def generar_sugerencia(request: Request):
     return JSONResponse({"sugerencia": result})
 
 if __name__ == '__main__':
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
